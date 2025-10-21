08da4971d7cacffbbe9cb560ee631a3d976a3eca
zaahmad <zaahmad@wpi.edu>
2025-10-21 19:10:45 -0400

    setup next in frontend folder

--- frontend/src/App.jsx
+++
@@ -1,8 +0,0 @@
-export default function App() {
-  return (
-    <div style={{ padding: 24 }}>
-      <h1>Recruit.me</h1>
-      <p>Frontend placeholder. Add routes and components.</p>
-    </div>
-  );
-}
