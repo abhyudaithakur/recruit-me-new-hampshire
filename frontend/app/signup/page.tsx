726081df307c7d88de952e3af93a774b7b7d6627
Lev Giffune <levgiffune@gmail.com>
2025-10-31 12:06:16 -0400

    login/signup frontend, skeletonized UI

---
+++ frontend/app/signup/page.tsx
@@ -0,0 +1,76 @@
+'use client'
+import React, { FormEvent } from 'react';
+// import { useRouter } from 'next/navigation';
+import axios from 'axios';
+import { useAuth } from '@/components/AuthProvider';
+
+const instance = axios.create({
+    baseURL:''
+  })
+
+const Login: React.FC = () => {
+//   const router = useRouter();
+  const [load, setLoad] = React.useState({visibility: 'hidden'} as React.CSSProperties)
+  const [err, setErr] = React.useState('');
+
+  const { setCredential, setUserType } = useAuth()
+
+  const handleSignup = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const username = document.getElementById("username") as HTMLInputElement;
+    const password = document.getElementById("password") as HTMLInputElement;
+    const verify = document.getElementById("verify-password") as HTMLInputElement;
+
+    if(password.value != verify.value){
+      setErr('Passwords do not match!')
+      return
+    }
+
+    setLoad({visibility: 'visible'})
+    instance.post('/signup', {"username":username.value, "password":password.value}).then(function (response) {
+      const status = response.data.statusCode;
+
+      // <MOCK> ***********************************************
+      document.cookie = 'credential=' + '12345'
+      setCredential('username')
+      document.cookie='userType=' + 'applicant'
+      setUserType(response.data.userType)
+      // </MOCK> ***********************************************
+
+      if (status == 200) {
+        document.cookie = 'credential=' + response.data.credential
+        setCredential(response.data.credential)
+        document.cookie='userType=' + response.data.userType
+        setUserType(response.data.userType)
+        
+      }else{
+        setErr(response.data.error);
+        setLoad({visibility: 'hidden'})
+      }
+    })
+    .catch(function (error: React.SetStateAction<string>) {
+        setErr('failed to log in: ' + error);
+        setLoad({visibility: 'hidden'})
+    })
+  };
+
+  
+  return(
+    <div className='content'>
+        <h2>Sign Up</h2>
+        <form onSubmit={handleSignup}>
+          <input id='username' placeholder='Username'/>
+          <input id='password' placeholder='Password' type='password'/>
+          <input id='verify-password' placeholder='Verify password' type='password'/>
+          <button type="submit">Sign Up</button>
+          {/*eslint-disable-next-line @next/next/no-img-element*/}
+          <img src='/loading-7528_128.gif' alt="" id='loading' style={load}/>
+          <label className="error">{err}</label>
+        </form>
+        <a href='/signup'>Already have an account? Log In!</a>
+    </div>
+  );
+}
+
+
+export default Login;
---
+++ frontend/components/AuthProvider.tsx
@@ -0,0 +1,48 @@
+'use client'
+import { createContext, Dispatch, SetStateAction, useContext, useEffect, useState } from "react";
+
+interface IAuthContext{
+    credential: string
+    setCredential: Dispatch<SetStateAction<string>>,
+    loading: boolean,
+    userType: string,
+    setUserType: Dispatch<SetStateAction<string>>,
+}
+
+const AuthContext = createContext<IAuthContext>({
+    credential: '',
+    setCredential: () => {},
+    loading: true,
+    userType: '',
+    setUserType: () => {}
+})
+
+export const useAuth = () => useContext(AuthContext)
+
+const AuthProvider = ({ children }: Readonly<{children: React.ReactNode}>) => {
+    const [credential, setCredential] = useState('')
+    const [loading, setLoading] = useState(true)
+    const [userType, setUserType] = useState('')
+
+    useEffect(() => {
+        const cred = document.cookie.split("; ").find((row) => row.startsWith("credential="))?.split("=")[1];
+        const ut = document.cookie.split("; ").find((row) => row.startsWith("userType="))?.split("=")[1];
+        if(cred){
+            // eslint-disable-next-line react-hooks/set-state-in-effect
+            setCredential(cred)
+            setUserType(ut ? ut : '')
+        }else{
+            setCredential('')
+            setUserType('')
+        }
+        setLoading(false)
+    }, [])
+
+    return(
+        <AuthContext.Provider value={{ credential, setCredential, loading, userType, setUserType}}>
+          {children}
+        </AuthContext.Provider>
+    );
+}
+
+export default AuthProvider
